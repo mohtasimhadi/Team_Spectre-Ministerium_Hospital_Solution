9c792b8c86dfb06bf40281af1216326d6989a5d7
MOHTASIM HADI RAFI <mohtasimhadi@gmail.com>
2020-03-10 00:53:09 +0600

    Rafi

---
+++ src/Spectre/MHS/com/PatientInformationDoctor.java
@@ -0,0 +1,10 @@
+package Spectre.MHS.com;
+
+import javax.swing.*;
+
+public class PatientInformationDoctor {
+    private JButton backButton;
+    private JButton updateButton;
+    private JButton referButton;
+    private JButton refreshButton;
+}
