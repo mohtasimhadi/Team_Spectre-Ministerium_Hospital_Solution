df94ab481e956cd914f25294d7550bb77e728243
Rafsan Quayes <rafsan.quayes@gmail.com>
2020-03-10 01:33:11 +0600

    Encryption (Needs more Work)

---
+++ src/Spectre/MHS/com/Encryption.java
@@ -0,0 +1,40 @@
+package Spectre.MHS.com;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+public class Encryption {
+
+    public String Encrypt(String message) throws NoSuchAlgorithmException {
+        return  Hash(message);
+    }
+    private String Hash(String message){
+        String output = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+
+            byte[] bytes = md.digest(message.getBytes());
+            StringBuilder s = new StringBuilder();
+            for(int i=0; i<bytes.length; i++){
+                s.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            output = s.toString();
+        }
+        catch (NoSuchAlgorithmException e){
+            e.printStackTrace();
+        }
+        return output;
+    }
+    /*private byte[] getSalt() throws NoSuchAlgorithmException {
+        SecureRandom n = SecureRandom.getInstance("SHA1PRNG");
+        byte[] salt = new byte[16];
+        n.nextBytes(salt);
+        return salt;
+    }*/
+
+    public static void main(String[] args) throws NoSuchAlgorithmException{
+        Encryption n = new Encryption();
+        System.out.println(n.Encrypt("password"));
+    }
+}
