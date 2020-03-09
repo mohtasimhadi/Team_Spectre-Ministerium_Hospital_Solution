bdf7279acc3f061fa145a60fd4417fc39ddf9c8a
Sinclair <borshon.sadafshahid@gmail.com>
2020-03-10 01:01:17 +0600

    Sadaf

--- src/Spectre/MHS/com/PathologistLocale.java
+++ src/Spectre/MHS/com/PathologistLocale.java
@@ -1,11 +1,14 @@
 package Spectre.MHS.com;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class PathologistLocale {
     private JButton logOutButton;
     private JButton patientInformationButton;
     private JPanel contentPanel;
+
     private JButton changePassword;
     private JLabel jID;
     private JLabel jName;
@@ -21,4 +24,33 @@ public class PathologistLocale {
     private JLabel jWorkingDays;
     private JLabel jWorkHours;
     private JLabel jRoomNo;
+
+
+    public PathologistLocale() {
+
+        JFrame jFrame = new JFrame("Pathologist Locale");
+        jFrame.setVisible(true);
+        jFrame.setResizable(false);
+        jFrame.setSize(1280, 720);
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jFrame.add(contentPanel);
+
+        logOutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jFrame.setVisible(false);
+                onLogOut();
+            }
+        });
+    }
+
+    void onLogOut(){
+        PathologistLogIn logIn = new PathologistLogIn();
+    }
+
+    /*public static void main(String[] args){
+        PathologistLocale pathologistLocale = new PathologistLocale();
+    }*/
+
+
 }
