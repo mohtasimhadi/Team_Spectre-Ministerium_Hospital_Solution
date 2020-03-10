483f7b8d415a817a21b18f81cf9804d6dc047572
MOHTASIM HADI RAFI <mohtasimhadi@gmail.com>
2020-03-10 21:07:56 +0600

    PatientInformationDoctor

--- src/Spectre/MHS/com/PatientInformationDoctor.java
+++ src/Spectre/MHS/com/PatientInformationDoctor.java
@@ -1,6 +1,8 @@
 package Spectre.MHS.com;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class PatientInformationDoctor {
     private JButton backButton;
@@ -18,4 +20,24 @@ public class PatientInformationDoctor {
     private JTextArea jPathologyTests;
     private JTextField jDateOfRelease;
     private JPanel contentPanel;
+
+    public PatientInformationDoctor() {
+        JFrame jFrame = new JFrame("Doctors Station");
+        jFrame.setVisible(true);
+        jFrame.setResizable(false);
+        jFrame.setSize(1280, 720);
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jFrame.add(contentPanel);
+
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                onBack();
+                jFrame.setVisible(false);
+            }
+        });
+    }
+    void onBack(){
+        DoctorStation doctorStation = new DoctorStation();
+    }
 }
