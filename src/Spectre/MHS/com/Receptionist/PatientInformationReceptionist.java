70fb9e2cc79ef15828244314494c0d51730279ca
Sinclair <borshon.sadafshahid@gmail.com>
2020-09-15 04:01:41 +0600

    Implemented Receptionist & Pathologist

--- src/Spectre/MHS/com/Receptionist/PatientInformationReceptionist.java
+++ src/Spectre/MHS/com/Receptionist/PatientInformationReceptionist.java
@@ -1,11 +1,15 @@
 package Spectre.MHS.com.Receptionist;
 
 import Spectre.MHS.com.OperationsNTools.Display;
-
+import Spectre.MHS.com.OperationsNTools.PatientInfo;
+import Spectre.MHS.com.OperationsNTools.SQLConnector;
 import javax.swing.*;
 
 public class PatientInformationReceptionist {
-    private JButton backButton;
+    private JButton jbackButton;
+    private JButton jviewButton;
+    private JButton jrefreshButton;
+    private JButton jupdateButton;
     private JPanel contentPanel;
     private JTextField jPatientID;
     private JLabel jName;
@@ -19,9 +23,7 @@ public class PatientInformationReceptionist {
     private JLabel jBloodGroup;
     private JTextArea jPrescription;
     private JTextArea jPathologyTests;
-    private JButton viewButton;
-    private JButton refreshButton;
-    private JButton updateButton;
+
     private final String userid;
     private final Display display = new Display("Patient Information", contentPanel);
 
@@ -29,13 +31,53 @@ public class PatientInformationReceptionist {
         this.userid = userid;
         display.DisplayOn();
 
-        backButton.addActionListener(e -> {
+        jbackButton.addActionListener(e -> {
             onBack();
             display.DisplayOff();
+        });
+
+        jrefreshButton.addActionListener(e -> {
+            onRefresh();
+        });
+
+        jviewButton.addActionListener(e -> {
+            onView();
+
+        });
+
+        jupdateButton.addActionListener(e -> {
+            onUpdate();
+
         });
     }
 
     void onBack(){
+        display.DisplayOff();
         new ReceptionistRange(userid);
     }
+    void onRefresh(){
+        display.DisplayOff();
+        new PatientInformationReceptionist(userid);
+    }
+    void onView(){
+        String patientID = jPatientID.getText();
+        String query = ("SELECT * FROM patient WHERE ID="+patientID);
+        PatientInfo patientInfo = new PatientInfo();
+        //if(())              //Rafi HELP
+            patientInfo.getPatientInfoReceptionist(query,jName,jAge,jGender,jDateOfAdmission,jDateOfAppointment,jDateOfRelease,jContactNo,jEmail,jBloodGroup,jPrescription,jPathologyTests);
+        //else
+        //    JOptionPane.showMessageDialog(null, "No Patient With That ID");
+    }
+
+    void onUpdate(){
+        String patientID = jPatientID.getText();
+        String dateOfRelease = jDateOfRelease.getText();
+        PatientInfo patientInfo = new PatientInfo();
+        String query = ("UPDATE patient SET DateOfRelease ='"+dateOfRelease+ "' WHERE ID ="+patientID);
+        //if(())              //Rafi HELP
+            patientInfo.updatePatientInfo(query);
+        //else
+        //    JOptionPane.showMessageDialog(null, "No Patient With That ID");
+    }
+
 }
