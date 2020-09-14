53ad474da4a566a0e7de887d9c8d54b75cec8491
mohtasimhadi <mohtasimhadi@iut-dhaka.edu>
2020-09-15 01:15:34 +0600

    Getting Started With ViewEmployee

---
+++ src/Spectre/MHS/com/Admin/ViewEmployee.java
@@ -0,0 +1,11 @@
+package Spectre.MHS.com.Admin;
+
+import javax.swing.*;
+
+public class ViewEmployee {
+    private JPanel contentPanel;
+    private JButton backButton, viewEmployeeButton, changePasswordButton, removeEmployeeButton;
+    private JTextField userID, name, dateOfBirth, address,  contactNo,  Gender,  email,  designation,  bloodGroup, joiningDate;
+
+
+}
