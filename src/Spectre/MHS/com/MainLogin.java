0da4c919c4090d5ac00bb317ad193c8caa800f6b
Sinclair <borshon.sadafshahid@gmail.com>
2020-09-17 03:33:15 +0600

    Main Login Panel -> Exit button

--- src/Spectre/MHS/com/MainLogin.java
+++ src/Spectre/MHS/com/MainLogin.java
@@ -1,11 +1,26 @@
 package Spectre.MHS.com;
 
 import javax.swing.*;
+import Spectre.MHS.com.OperationsNTools.Display;
 
 public class MainLogin {
-    private JPanel panel1;
-    private JComboBox comboBox1;
+    private JPanel contentPanel;
+    private JComboBox loginType;
     private JButton selectButton;
     private JButton exitButton;
+
+    private final Display display = new Display("Log In Selection", contentPanel);
+
+    public MainLogin() {
+        display.displayOn();
+
+        exitButton.addActionListener(e -> onExit());
+
+        
+    }
+
+    void onExit(){
+        System.exit(0);
+    }
 
 }
