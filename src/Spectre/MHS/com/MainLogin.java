9a07b24cac47d33abde67a3940b5ccb6183eea6e
Sinclair <borshon.sadafshahid@gmail.com>
2020-09-17 03:27:51 +0600

    Main Login Panel

---
+++ src/Spectre/MHS/com/MainLogin.java
@@ -0,0 +1,11 @@
+package Spectre.MHS.com;
+
+import javax.swing.*;
+
+public class MainLogin {
+    private JPanel panel1;
+    private JComboBox comboBox1;
+    private JButton selectButton;
+    private JButton exitButton;
+
+}
